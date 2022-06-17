2228d166539c2beae2e981e9fc1172a368dcd5a9
ndjerrou <n.djerrou@gmail.com>
2022-06-17 09:05:01 +0100

    node explanation - B

---
+++ modules/index.js
@@ -0,0 +1,5 @@
+const path = require('path'); // chargement d'un module
+
+console.log(path.dirname(__filename));
+
+console.log(path.join(__filename, '../sample'));
---
+++ sample/index.js
@@ -0,0 +1,7 @@
+function sum(a, b) {
+  return a + b;
+}
+
+const res = sum(2, 3);
+
+console.log('2 + 3 = ', res);
