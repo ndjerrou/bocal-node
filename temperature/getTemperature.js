bf8e0807a76f152ac6885b4641b2705dad6f6471
ndjerrou <n.djerrou@gmail.com>
2022-06-20 11:33:39 +0100

    temperature done

--- temperature/getTemperature.js
+++ temperature/getTemperature.js
@@ -1,6 +1,11 @@
-// atterir ici ???
+const axios = require('axios');
 
-console.log('je suis dans temperature');
-function getTemperature(coord) {
-  console.log(coord); // ??
+async function getTemperature(coordinates) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.longitude}&appid=75df4771c32af58b732d3f749b83215d&units=metric`;
+
+  const resultat = await axios.get(url);
+
+  return resultat.data.main.temp;
 }
+
+module.exports = getTemperature;
--- temperature/index.js
+++ temperature/index.js
@@ -1,14 +1,16 @@
 const getCoordinates = require('./getCoordinates.js');
 
+const getTemperature = require('./getTemperature');
+
 // 1 - Getting the city
 const city = process.argv[2];
 
 // // 2 - Getting the coordinates - using .then method to handle promise
-getCoordinates(city).then(function (resultat) {
-  console.log(resultat);
+getCoordinates(city).then(function (coordinates) {
+  // getting coord from getCoordinates
+  //const temperature = getTemperature(coordinates); // WRONG : need to wait for the result
+
+  getTemperature(coordinates).then(function (temperature) {
+    console.log(`La température à ${city} est de : ${temperature}°`);
+  });
 });
-
-// // 3 - Getting the temperature
-// const temperature = getTemperature(coordinates);
-
-// console.log(temperature);
