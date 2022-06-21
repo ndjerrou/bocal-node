5cc4e822b962f49f68915e63dcbbbb1ae1b87cb3
ndjerrou <n.djerrou@gmail.com>
2022-06-21 08:57:09 +0100

    First API

--- express-app/server.js
+++ express-app/server.js
@@ -1,19 +1,55 @@
-// framework express - module créé pour nodejs
-
-// Création d'un serveur web avec express
-
-// 2 endpoints :
-
-// /homepage ==> <h1>hello into my website</h1>
-
-// /city ==> <p>Vous habitez à [city]</p>
-
 const express = require('express');
 
 const app = express();
 
+app.use(express.json()); //middleware
+
+const products = [
+  { id: 1, name: 'xiaomi x11', price: 30 },
+  { id: 2, name: 'xiaomi x12', price: 20 },
+];
+
+// combos / RESTFull --> comment architecture notre API
+
+// [GET /, GET /:id, POST /, UPDATE /:id, DELETE /:id]
+
+// combo = VERB + ENDPOINT
+
 app.get('/', (req, res) => {
-  res.send('<p>Je suis un paragraphe venant du serveur !!!</p>');
+  res.send('Bienvenue sur la homepage !');
+});
+
+// route parameters
+app.get('/cities/:city', (req, res) => {
+  console.log(req.params);
+
+  res.send(`Vous habitez à : ${req.params.city}`);
+});
+
+// query string parameters
+app.get('/products', (req, res) => {
+  const filter = req.query.filterBy;
+
+  if (filter === 'price') products.sort((a, b) => (a.price < b.price ? -1 : 1));
+
+  res.send(products);
+});
+
+app.post('/products', (req, res) => {
+  console.log(req.body);
+
+  products.push({
+    id: products.length + 1,
+    name: req.body.name,
+    price: req.body.price,
+  });
+
+  res.send({
+    success: 'Great ! ',
+    data: {
+      products,
+    },
+  });
 });
 
 app.listen(3000, () => console.log('Server listenning on port 3000'));
