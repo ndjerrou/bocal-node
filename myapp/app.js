4ce59249d11beb190d1447c90d1285e6ec1328d2
ndjerrou <n.djerrou@gmail.com>
2022-06-23 12:46:39 +0100

    added cors package

--- myapp/app.js
+++ myapp/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -9,6 +10,7 @@ const reseller = require('./routes/reseller');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
